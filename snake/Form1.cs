2e40cedcda07ce02fbb39a7bcf69cd42514eecd1
Coskun <volkan.coskun@afry.com>
2021-09-08 11:34:31 +0200

    refactored classes into Form1.cs

--- snake/Form1.cs
+++ snake/Form1.cs
@@ -13,12 +13,36 @@ static class Constants
     public const int gameTimerInterval = 500;
 }
 
+class snakeCircleBody
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public snakeCircleBody()
+    {
+        X = 0;
+        Y = 0;
+    }
+}
+
+class initialSettings
+{
+    public static int Width { get; set; }
+    public static int Height { get; set; }
+    public static string direction;
+    public initialSettings()
+    {
+        Width = 15;
+        Height = 15;
+        direction = "LEFT";
+    }
+}
+
 namespace snake
 {
     public partial class Form1 : Form
     {
-        private List<Circle> Snake = new List<Circle>();
-        private Circle food = new Circle();
+        private List<snakeCircleBody> Snake = new List<snakeCircleBody>();
+        private snakeCircleBody food = new snakeCircleBody();
         int maxWidth;
         int maxHeight;
         Random rand = new Random();
@@ -26,25 +50,25 @@ namespace snake
         public Form1()
         {
             InitializeComponent();
-            new setting();
+            new initialSettings();
         }
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Left && setting.direction != "RIGHT")
+            if (e.KeyCode == Keys.Left && initialSettings.direction != "RIGHT")
             {
-                setting.direction = "LEFT";
+                initialSettings.direction = "LEFT";
             }
-            if (e.KeyCode == Keys.Right && setting.direction != "LEFT")
+            if (e.KeyCode == Keys.Right && initialSettings.direction != "LEFT")
             {
-                setting.direction = "RIGHT";
+                initialSettings.direction = "RIGHT";
             }
-            if (e.KeyCode == Keys.Up && setting.direction != "DOWN")
+            if (e.KeyCode == Keys.Up && initialSettings.direction != "DOWN")
             {
-                setting.direction = "UP";
+                initialSettings.direction = "UP";
             }
-            if (e.KeyCode == Keys.Down && setting.direction != "UP")
+            if (e.KeyCode == Keys.Down && initialSettings.direction != "UP")
             {
-                setting.direction = "DOWN";
+                initialSettings.direction = "DOWN";
             }
         }
         private void StartGame(object sender, EventArgs e)
@@ -79,7 +103,7 @@ namespace snake
         }
         private void updateSnakeHead()
         {
-            switch (setting.direction)
+            switch (initialSettings.direction)
             {
                 case "LEFT":
                     Snake[Constants.headPos].X--;
@@ -123,30 +147,30 @@ namespace snake
 
             for (int i = 0; i < Snake.Count; i++)
             {
-                canvas.FillEllipse((i == Constants.headPos) ? Brushes.Black : Brushes.DarkGreen, new Rectangle(Snake[i].X * setting.Width, Snake[i].Y * setting.Height, setting.Width, setting.Height));
+                canvas.FillEllipse((i == Constants.headPos) ? Brushes.Black : Brushes.DarkGreen, new Rectangle(Snake[i].X * initialSettings.Width, Snake[i].Y * initialSettings.Height, initialSettings.Width, initialSettings.Height));
             }
-            canvas.FillEllipse(Brushes.DarkRed, new Rectangle(food.X * setting.Width, food.Y * setting.Height, setting.Width, setting.Height));
+            canvas.FillEllipse(Brushes.DarkRed, new Rectangle(food.X * initialSettings.Width, food.Y * initialSettings.Height, initialSettings.Width, initialSettings.Height));
         }
 
         private void RestartGame()
         {
-            maxWidth = picCanvas.Width / setting.Width - 1;
-            maxHeight = picCanvas.Height / setting.Height - 1;
+            maxWidth = picCanvas.Width / initialSettings.Width - 1;
+            maxHeight = picCanvas.Height / initialSettings.Height - 1;
 
             Snake.Clear();
 
             startButton.Enabled = false;
            
-            Circle head = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight)};
+            snakeCircleBody head = new snakeCircleBody { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight)};
             Snake.Add(head); // adding the head part of the snake to the list
 
             for (int i = 0; i < 3; i++)
             {
-                Circle body = new Circle();
+                snakeCircleBody body = new snakeCircleBody();
                 Snake.Add(body);
             }
 
-            food = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
+            food = new snakeCircleBody { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
 
             gameTimer.Start();
         }
@@ -172,7 +196,7 @@ namespace snake
         {
             if (Snake[Constants.headPos].X == food.X && Snake[Constants.headPos].Y == food.Y)
             {
-                Circle newBody = new Circle
+                snakeCircleBody newBody = new snakeCircleBody
                 {
                     X = Snake[Snake.Count - 1].X,
                     Y = Snake[Snake.Count - 1].Y
@@ -188,7 +212,7 @@ namespace snake
                 }
                 Snake.Add(newBody);
 
-                food = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
+                food = new snakeCircleBody { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
             }
         }
     }
